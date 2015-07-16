ed8e32745e39ed29bab4a796e76f2f7cfcdb9f9c
Mehdi Benadda <me@mbenadda.com>
2015-07-16 19:23:34 +0200

    Update content-for to wait for an $apply to look for yielders
    When using uiContenFor in a template with an ngController, to project
    content in an uiYieldTo located inside a directive, a race condition
    prevents the uiContentFor from working: it executes before the uiYieldTo
    has had time to register with Capture.
    
    A simple solve is to do it in 2 trys: one right when setContentFor is called,
    the other one in an $applyAsync callback so it is delayed enough for the
    directive to have been compiled.

--- src/js/core/capture.js
+++ src/js/core/capture.js
@@ -160,12 +160,22 @@
          },
 
          setContentFor: function(name, content, scope) {
-           var b = yielders[name];
-           if (!b) {
-             return;
-           }
-           b.element.html(content);
-           $compile(b.element.contents())(scope);
+          // Attempt to effect the content-for ASAP
+          if (yielders[name]) {
+            var yielder = yielders[name];
+            yielder.element.html(content);
+            $compile(yielder.element.contents())(scope);
+          }
+          // If we could not do it on the spot, wait for an apply cycle and retry
+          else {
+            scope.$applyAsync(function () {
+              if (yielders[name]) {
+                var yielder = yielders[name];
+                yielder.element.html(content);
+                $compile(yielder.element.contents())(scope);
+              }
+            });
+          }
          }
 
        };
